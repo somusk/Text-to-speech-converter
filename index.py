3ac6947d3d8ac991137759d05ee6f814819a5f3b
somusk <113496311+somusk@users.noreply.github.com>
2024-04-02 14:39:18 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,12 @@
+from gtts import gTTS 
+from playsound import playsound 
+
+fileName = "sample.mp3"
+textInput = input("Enter any text: ")
+language = input("Enter language type: ")
+
+convertingAudio = gTTS(text=textInput, lang=language, slow=False)
+convertingAudio.save(fileName)
+playsound(fileName)
+
+
